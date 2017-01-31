039ef5dcab084267cbc02c53ac8a0d1195538ba8
Ryan Ingebritson <ryan.ingebritson@gmail.com>
2017-01-31 11:20:03 -0600

    Updated to work with Instagram

--- JSC/js/main.js
+++ JSC/js/main.js
@@ -128,8 +128,24 @@ function getComments() {
 getComments();
 
 
-
-
+var token = 'd59212ed8c7c4504b59626af27fcc4be',
+    num_photos = 3;
+ 
+$.ajax({
+    url: 'https://api.instagram.com/v1/users/self/media/recent',
+    dataType: 'jsonp',
+    type: 'GET',
+    data: {access_token: token, count: num_photos},
+    success: function(data){
+        console.log(data);
+        for( x in data.data ){
+            $('ul').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>');
+        }
+    },
+    error: function(data){
+        console.log(data);
+    }
+});
 
 
 // initialize the configuration of map
