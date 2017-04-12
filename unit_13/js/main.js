e0ef789eea777c95021a9b22b8d9f0929b2c8c6f
Ryan Ingebritson <ryan.ingebritson@gmail.com>
2017-04-11 19:04:09 -0500

    Revert "Removed large file"
    This reverts commit d7aef9b940980ab4673fb0c8b5b7381947e7d210.
    
    # Conflicts:
    #	.DS_Store

--- unit_13/js/main.js
+++ unit_13/js/main.js
@@ -3,11 +3,6 @@ $(function(){
   $('#mainwrapper').fadeIn(3000);
 });
 
-// create a clone of the menu, 
-// which we'll only reveal when the user scrolls past a certain position
-$('nav').addClass('original').clone().insertAfter('nav').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
-
-scrollIntervalID = setInterval(stickIt, 10);
 
 var token = '402445702.d59212e.80b303912d3f42269f4b9c04e3b7ecdd',
     num_photos = 3;
@@ -20,7 +15,7 @@ $.ajax({
     success: function(data){
         console.log(data);
         for( x in data.data ){
-            $('ul').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>');
+            $('#rudr_instafeed').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>');
         }
     },
     error: function(data){
