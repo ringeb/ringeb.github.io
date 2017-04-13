efae03f7ae25af853aea6c25e93c7a8cbb7d87da
Ryan Ingebritson <ryan.ingebritson@gmail.com>
2017-04-13 16:35:58 -0500

    Made a footer

--- unit_13/js/main.js
+++ unit_13/js/main.js
@@ -28,3 +28,20 @@ var div = document.getElementById('box1');
 div.style.display = 'none';
 
 }
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      $(".anchor-link").removeClass("active");
+      $(this, ".anchor-link").addClass("active");
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
