884f4acde6ca68972658ffa35c5cfb296efaaf03
Ryan Ingebritson <ryan.ingebritson@gmail.com>
2017-04-30 18:36:26 -0500

    Lets see if this works

--- unit_13/js/main.js
+++ unit_13/js/main.js
@@ -4,24 +4,11 @@ $(function(){
 });
 
 
-var token = '402445702.d59212e.80b303912d3f42269f4b9c04e3b7ecdd',
-    num_photos = 3;
- 
-$.ajax({
-    url: 'https://api.instagram.com/v1/users/self/media/recent',
-    dataType: 'jsonp',
-    type: 'GET',
-    data: {access_token: token, count: num_photos},
-    success: function(data){
-        console.log(data);
-        for( x in data.data ){
-            $('#rudr_instafeed').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>');
-        }
-    },
-    error: function(data){
-        console.log(data);
-    }
-});
+var frmvalidator  = new Validator("contactform");
+frmvalidator.addValidation("name","req","Please provide your name");
+frmvalidator.addValidation("email","req","Please provide your email");
+frmvalidator.addValidation("email","email",
+  "Please enter a valid email address");
 
 hideMenu = function() {
 var div = document.getElementById('box1');
