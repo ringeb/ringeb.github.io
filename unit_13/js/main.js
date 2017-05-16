26ec6b79fe6e0074e44ff20f4eb599885552e45c
Ryan Ingebritson <ryan.ingebritson@gmail.com>
2017-05-15 21:45:45 -0500

    Changes

--- unit_13/js/main.js
+++ unit_13/js/main.js
@@ -4,17 +4,8 @@ $(function(){
 });
 
 
-var frmvalidator  = new Validator("contactform");
-frmvalidator.addValidation("name","req","Please provide your name");
-frmvalidator.addValidation("email","req","Please provide your email");
-frmvalidator.addValidation("email","email",
-  "Please enter a valid email address");
 
-hideMenu = function() {
-var div = document.getElementById('box1');
-div.style.display = 'none';
 
-}
 $('aside a:first').addClass('activeAside');
 
 $("aside a").on('click', function(event) {
@@ -48,5 +39,26 @@ $(window).scroll(function() {
 
 }).scroll();
 
+// this is for the modal image
+// Get the modal
+var modal = document.getElementById('myModal');
 
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
 
